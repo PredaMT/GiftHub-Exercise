0cdad3e6cfb1678cb02e52413622d4b87db5033a
PredaMT <predabussines@yahoo.com>
2024-10-24 12:31:18 +0100

    Initial Commit

---
+++ GiftHub Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GiftHub Exercise
+//
+//  Created by Mihai Preda on 24/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GiftHub Exercise/GiftHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftHub_ExerciseApp.swift
+//  GiftHub Exercise
+//
+//  Created by Mihai Preda on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GiftHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
